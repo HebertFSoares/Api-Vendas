5d3c4d11c484e1d89924a4d0ff967b9720300ad5
Hebert Soares <hebertsoaresof@gmail.com>
2023-12-22 19:09:53 -0300

    Criando rota de teste

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,13 @@
-console.log("Hello Dev!");
+import express from "express";
+import cors from "cors";
+import router from "./routes/routes";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(router)
+app.listen(3333, () => {
+  console.log("Server started!")
+})
